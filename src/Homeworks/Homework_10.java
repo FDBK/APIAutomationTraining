8bf4cd3a888015dcc75795da3986db9238460f66
FDBK <single-coil@ya.ru>
2022-10-19 11:49:55 +0700

    Homework 3.1 (10)
    Проверка длины строки (>15).

---
+++ src/Homeworks/Homework_10.java
@@ -0,0 +1,18 @@
+package Homeworks;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Homework_10 {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Short string", "String with more than 15 symbols"})
+    public void testCheckLength(String string) {
+
+        assertTrue(string.length() > 15, "Error! The string's length is less than 15, it's " + string.length());
+
+    }
+
+}
