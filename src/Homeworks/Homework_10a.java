54c9074a64795274227509eb549a91c42a7ae9c4
FDBK <single-coil@ya.ru>
2022-10-19 12:15:04 +0700

    Добавил точку для красоты )

--- src/Homeworks/Homework_10a.java
+++ src/Homeworks/Homework_10a.java
@@ -11,7 +11,7 @@ public class Homework_10a {
     @ValueSource(strings = {"Short string", "Fifteen symbols", "String with more than 15 symbols"})
     public void testCheckLength(String string) {
 
-        assertTrue(string.length() > 15, "Error! The string's length is not greater than 15, it's " + string.length());
+        assertTrue(string.length() > 15, "Error! The string's length is not greater than 15, it's " + string.length() + ".");
 
     }
 
