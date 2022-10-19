e2957ba64418061523d4c47e101cf2a8b2e217d5
FDBK <single-coil@ya.ru>
2022-10-19 12:16:04 +0700

    Homework 3.2 (11)
    Проверка значения HomeWork cookie.

---
+++ src/Homeworks/Homework_11a.java
@@ -0,0 +1,23 @@
+package Homeworks;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Homework_11a {
+
+    @Test
+    public void testAssertHomeworkCookie() {
+
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        String homeworkCookie = response.getCookie("HomeWork");
+        assertEquals("hw_value", homeworkCookie, "Error! Incorrect cookie value: " + homeworkCookie + ".");
+
+    }
+
+}
